6484b9b5f44f976ab01b06cc9c57a03d5e17b435
MichelChaghoury <michel.e.chaghoury@gmail.com>
2023-06-16 19:25:58 +0300

    Add README.md File. Add Contains Duplicate Question with Solution.

--- README.md
+++ README.md
@@ -2,22 +2,22 @@
 
 ## Technical Interview Questions
 
-A collection of the most common and important _data structures_ and _algorithms_ asked during **technical interviews**.
+This repository contains a comprehensive collection of common and important data structures and algorithms that are frequently asked during technical interviews.
 
-Each _class_ represents a problem with its respective _explanation_ as _comments_ and solutions.  
-_Problems_ are organized into _packages_, where each package contains problems related to the same **concepts** and **Data Structure**.
+Each class represents a problem and includes comments and solutions to provide explanations. The problems are organized into packages, with each package further divided into three sub-packages based on the problem's difficulty level: Easy, Medium, and Hard.  
+These sub-packages contain problems related to the same concepts and data structures.
 
 The project is structured as follows:
 
-### main package
+### Main Package
 
-- array
-- pointer
-- window-slider
-- stack
-- binary-search
-- dynamic-programming
+- Array
+- Pointer
+- Window Slider
+- Stack
+- Binary Search
+- Dynamic Programming
 
 ### Behavioral Interview Questions
 
-The `FAQ.md` file is a collection of the most common and frequently asked questions with answers during **behavioral interviews**.
+The `FAQ.md` file includes a compilation of the most common and frequently asked questions along with their answers, specifically focusing on behavioral interviews.
--- src/main/array/easy/ContainsDuplicate.java
+++ src/main/array/easy/ContainsDuplicate.java
@@ -1,42 +1,39 @@
-package main.array;
+package main.array.easy;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class ContainsDuplicate {
-
     /*
-     * LeetCode 217 | Contains Duplicates
+     * 217. Contains Duplicate
      * Level: Easy
-     *
      * Link: https://leetcode.com/problems/contains-duplicate/
-     *
+     * 
      * Given an integer array nums, return true if any value appears at least twice
      * in the array, and return false if every element is distinct.
-     *
+     * 
      * Example 1:
      * Input: nums = [1,2,3,1]
      * Output: true
-     *
+     * 
      * Example 2:
      * Input: nums = [1,2,3,4]
      * Output: false
-     *
+     * 
      * Example 3:
      * Input: nums = [1,1,1,3,3,4,3,2,4,2]
      * Output: true
-     *
-     *
+     * 
      * Constraints:
      * 1 <= nums.length <= 105
      * -109 <= nums[i] <= 109
      */
 
     /**
-     * check if an array contains duplicate values
-     *
-     * @param nums array to check for duplicates
-     * @return True when duplicates were found. False when containing unique values
+     * Checks if the given integer array contains any duplicate values.
+     * 
+     * @param nums The integer array to check for duplicates.
+     * @return true if any value appears at least twice, false otherwise.
      */
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>();
@@ -51,4 +48,5 @@ public class ContainsDuplicate {
 
         return false;
     }
+
 }
