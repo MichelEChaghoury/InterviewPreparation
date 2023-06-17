e48aaf773b4c41d891bda794f621007a3427b6be
MichelChaghoury <michel.e.chaghoury@gmail.com>
2023-06-17 16:59:07 +0300

    Add Valid Anagram Problem with Solution.

---
+++ src/main/array/easy/ValidAnagram.java
@@ -0,0 +1,60 @@
+package main.array.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidAnagram {
+    /*
+     * 242. Valid Anagram
+     * Level: Easy
+     * Link: https://leetcode.com/problems/valid-anagram/
+     * 
+     * Given two strings s and t, return true if t is an anagram of s, and false
+     * otherwise.
+     * 
+     * An Anagram is a word or phrase formed by rearranging the letters of a
+     * different word or phrase, typically using all the original letters exactly
+     * once.
+     * 
+     * Example 1:
+     * Input: s = "anagram", t = "nagaram"
+     * Output: true
+     * 
+     * Example 2:
+     * Input: s = "rat", t = "car"
+     * Output: false
+     * 
+     * Constraints:
+     * 1 <= s.length, t.length <= 5 * 104
+     * s and t consist of lowercase English letters.
+     */
+
+    /**
+     * Checks if the given Strings are anagrams.
+     *
+     * @param s the first String
+     * @param t the second String
+     * @return true if they are anagrams, false otherwise
+     */
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] first = s.toCharArray();
+        char[] second = t.toCharArray();
+
+        Map<Character, Integer> firstMap = new HashMap<>();
+        Map<Character, Integer> secondMap = new HashMap<>();
+
+        for (int i = 0; i < t.length(); i++) {
+            char firstChar = first[i];
+            char secondChar = second[i];
+
+            firstMap.put(firstChar, firstMap.getOrDefault(firstChar, 0) + 1);
+            secondMap.put(secondChar, secondMap.getOrDefault(secondChar, 0) + 1);
+        }
+
+        return firstMap.equals(secondMap);
+    }
+}
